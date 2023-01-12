56de2bbb7be2ed1f407f62a241a316022cc5cfb5
hrishizone <hrishikeshdongare12@gmail.com>
2023-01-12 13:20:30 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Goliath National Bank</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-landmark"></i> Goliath National Bank </a>
+
+    <nav class="navbar">
+        <a href="#home">HOME</a>
+        <a href="#about">ABOUT</a>
+        <a href="#services">USERS</a>
+        <a href="#services">TRANSACTIONS</a>
+    </nav>
+
+
+   
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3> <span></span> </h3>
+        <a href="#about" class="btn">Explore</a>
+    </div>
+
+    <!-- <img src="image/bottom_wave.png" class="wave" alt=""> -->
+
+</section>
+
+<!-- home section ends -->
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+    <div class="image">
+        <img src="image/logo.png" alt="">
+    </div>
+
+    <div class="content">
+        <h3>Pure  <span>Banking,</span> Nothing Else</h3>
+        <p>BEWARE OF CYBER FRAUDS! DO NOT SHARE YOUR CARD/PIN/OTP/CVV WITH ANYONE. DO NOT CLICK ON UNKNOWN LINKS
+            RECEIVED ON PHONE/EMAIL.</p>
+        <a href="#" class="btn">read more</a>
+    </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+    <h1 class="heading"> our <span>services</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-landmark"></i>
+            <h3>TRANSFER MONEY</h3>
+            <a href="customer.php" class="btn">Bank transfer</a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-address-book"></i>
+            <h3>TRANSACTIONS</h3>
+            <a href="transaction.php" class="btn">History</a>
+        </div>
+
+       
+    </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- plan section starts  -->
+
+<section class="plan" id="plan">
+
+    <h1 class="heading"> Our Premium <span>Credit</span> Cards </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3 class="title"></h3>
+            <h3 class="day">GNB Card ELITE</h3>
+            <i class="fas fa-chess-king icon"></i>
+            <div class="list">
+                <p> Welcome e-Gift Voucher worth Rs. 5,000 on joining <span class="fas fa-check"></span> </p>
+                <p> Get free movie tickets worth Rs. 6,000 every year <span class="fas fa-check"></span> </p>
+                <p> Earn upto 50,000 Bonus Reward Points worth Rs. 12,500/year <span class="fas fa-check"></span> </p>
+                <p> Complimentary membership to Club Vistara and Trident Privilege program <span class="fas fa-check"></span> </p>
+            </div>
+            <div class="amount"><span>₹</span>4999<span>+Taxes</span></div>
+            <a href="#" class="btn">Apply Now</a>
+        </div>
+
+        <div class="box">
+            <h3 class="title"></h3>
+            <h3 class="day">Doctor's Card</h3>
+            <i class="fas fa-user-md icon"></i>
+            <div class="list">
+                <p>Professional Indemnity Insurance cover of Rs. 20 Lakhs<span class="fas fa-check"></span> </p>
+                <p>e-Gift Voucher worth Rs. 1,500 on joining<span class="fas fa-check"></span> </p>
+                <p>5X Reward Points on Medical Supplies, Travel Bookings, International Spends and Doctors’ Day<span class="fas fa-check"></span> </p>
+                <p>e-Gift Voucher worth Rs. 5,000 on annual spends of Rs. 5 Lakhs<span class="fas fa-check"></span> </p>
+            </div>
+            <div class="amount"><span>₹</span>1,499<span>+Taxes</span></div>
+            <a href="#" class="btn">Apply Now</a>
+        </div>
+
+        <div class="box">
+            <h3 class="title"></h3>
+            <h3 class="day">Cashback Card</h3>
+            <i class="fas fa-gift icon"></i>
+            <div class="list">
+                <p>No joining Fees<span class="fas fa-check"></span> </p>
+                <p>5% cashback on every* online spend without any merchant restriction<span class="fas fa-check"></span> </p>
+                <p>1% cashback on all* offline spends and utility bill payments<span class="fas fa-check"></span> </p>
+                <p>Card Cashback will be auto-credited to your SBI Card<span class="fas fa-check"></span> </p>
+            </div>
+            <div class="amount"><span>₹</span>999<span>+Taxes</span></div>
+            <a href="#" class="btn">Apply Now</a>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- plan section ends -->
+
+<section class="contact" id="contact">
+
+    <div class="image">
+        <img src="image/contact.png" alt="">
+    </div>
+
+    <form action="">
+        <h3>contact us</h3>
+        <input type="text" placeholder="Name" class="box">
+        <input type="email" placeholder="Email" class="box">
+        <input type="tumber" placeholder="Contact Number" class="box">
+        <textarea name="" placeholder="Message" id="" cols="30" rows="10"></textarea>
+        <input type="submit" value="send message" class="btn">
+    </form>
+
+</section>
+
+<section class="footer">
+
+    <!-- <img src="image/top_wave.png" alt=""> -->
+
+    <div class="share">
+        <a href="#" class="btn"> <i class="fab fa-facebook-f"></i> GNB Official </a>
+        <a href="#" class="btn"> <i class="fab fa-twitter"></i> GNB Official </a>
+        <a href="#" class="btn"> <i class="fab fa-instagram"></i> GNB Official </a>
+        <a href="#" class="btn"> <i class="fab fa-linkedin"></i> GNB Official </a>
+        <a href="#" class="btn"> <i class="fab fa-whatsapp"></i> Contact Us </a>
+    
+    </div>
+
+    <div class="credit"><span> </span> </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
